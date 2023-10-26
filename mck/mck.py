d6a9606b5413c3662d6e49a3f3ed5639b03a6e8f
nathan hensel <nathanihensel@gmail.com>
2023-10-25 22:37:21 -0500

    fetch ipv4 from terraform state

--- mck/mck.py
+++ mck/mck.py
@@ -1,12 +1,21 @@
 import argparse
 import subprocess
 import os
+import json
 
 
 def run_cmd(cmd):
     err = subprocess.call(cmd, shell=True)
     if err != 0:
         raise RuntimeError("command `{}` failed".format(cmd))
+
+
+def parse_instances():
+    result = subprocess.run(["tofu", "show", "-json"], check=True, capture_output=True)
+    state = json.loads(result.stdout)
+    for i in state["values"]["root_module"]["resources"]:
+        if i["address"] in "digitalocean_droplet.instance":
+            print(i["values"]["ipv4_address"])
 
 
 def main():
@@ -21,3 +30,4 @@ def main():
         run_cmd("tofu destroy")
     else:
         run_cmd("tofu apply")
+        parse_instances()
