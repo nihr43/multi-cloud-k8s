5a41a3bd22b089a73d6954ee19d6cda599b93d56
nathan hensel <nathanihensel@gmail.com>
2023-10-26 00:32:08 -0500

    increase memory

--- instances.tf
+++ instances.tf
@@ -2,6 +2,6 @@ resource "digitalocean_droplet" "instance" {
   image    = "debian-12-x64"
   name     = "test"
   region   = "nyc1"
-  size     = "s-1vcpu-1gb"
+  size     = "s-1vcpu-2gb"
   ssh_keys = [digitalocean_ssh_key.default.fingerprint]
 }
