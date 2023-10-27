a4213055b0aa5d60e67a8a35c07b2a10fbe5085d
nathan hensel <nathanihensel@gmail.com>
2023-10-26 23:34:55 -0500

    pull aws overhead into seperate file

---
+++ aws_meta.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-west-2a"
+}
+
+resource "aws_default_vpc" "mainvpc" {
+}
+
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_default_vpc.mainvpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- instances.tf
+++ instances.tf
@@ -16,47 +16,13 @@ resource "linode_instance" "instance" {
   count           = 1
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t1.micro"
   key_name               = "opentofu"
   vpc_security_group_ids = [aws_default_security_group.default.id]
-  count = 1
+  count                  = 1
   tags = {
     Name = "mck-k8s-aws-${count.index}"
-  }
-}
-
-resource "aws_default_subnet" "default_az1" {
-  availability_zone = "us-west-2a"
-}
-
-resource "aws_default_vpc" "mainvpc" {
-}
-
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_default_vpc.mainvpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
